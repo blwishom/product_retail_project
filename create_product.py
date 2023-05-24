ef58e55af6064a92c46f0a29d9dea38441edd4f7
Kelly Ortega <InvaderLynn10@gmail.com>
2023-05-24 11:49:16 -0400

    first pass at making the product table

---
+++ create_product.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://<mysql_username>:<mysql_password>@<mysql_host>:<mysql_port>/<mysql_db>'
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    __tablename__ = "Product"
+    product_id = db.Column(db.Integer, primary_key=True)
+    product_desc = db.Column(db.String(255))
+    in_stock = db.Column(db.Integer, nullable=False )
+    product_price = db.Column(db.Integer, nullable=False )
+    pproduct_category = db.Column(db.String(255))
+
+    def create(self):
+      db.session.add(self)
+      db.session.commit()
+      return self
+    def __init__(self,product_desc,in_stock,product_price,product_category):
+        self.product_desc = product_desc
+        self.in_stock = in_stock
+        self.product_price = product_price
+        self.product_category = product_category
+    def __repr__(self):
+        return '' % self.id
+db.create_all()
