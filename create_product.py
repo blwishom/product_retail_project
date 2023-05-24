716c2bb2813e8084bbf5455ab600746223cb71be
Kelly Ortega <InvaderLynn10@gmail.com>
2023-05-24 13:00:01 -0400

    fixed a silly typo x2

--- create_product.py
+++ create_product.py
@@ -12,14 +12,14 @@ class Product(db.Model):
     product_desc = db.Column(db.String(255))
     in_stock = db.Column(db.Integer, nullable=False )
     product_price = db.Column(db.Integer, nullable=False )
-    product_category = db.Column(db.String(255))
+    product_category = db.Column(db.Float())
 
     def create(self):
       db.session.add(self)
       db.session.commit()
       return self
-    def __init__(self,produt_name,product_desc,in_stock,product_price,product_category):
-        self.produt_name = produt_name
+    def __init__(self,product_name,product_desc,in_stock,product_price,product_category):
+        self.product_name = product_name
         self.product_desc = product_desc
         self.in_stock = in_stock
         self.product_price = product_price
